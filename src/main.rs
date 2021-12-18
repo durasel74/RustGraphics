3f5c415b2e9eaeab7c43cf72fdec29856269554a
durasel74 <diman174cheb@mail.ru>
2021-12-18 22:49:12 +0500

    Clear window

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,33 @@
-//extern crate sdl2;
-// pub mod main_old;
-// pub mod render_gl;
-//main_old::remain();
-
 use sdl2;
-
-macro_rules! wait {
-    () => {
-        std::io::stdin().read_line(&mut String::new()).unwrap();
-    };
-}
+use gl;
 
 fn main() {
+    // Initialization
     let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let window = video_subsystem
+        .window("RustGraphics", 800, 600)
+        .opengl()
+        .resizable()
+        .build()
+        .unwrap();
+    let _gl_context = window.gl_create_context().unwrap();
+    gl::load_with(|s| video_subsystem
+        .gl_get_proc_address(s) as *const std::os::raw::c_void);
 
+    unsafe { gl::ClearColor(0.2, 0.5, 0.6, 1.0); }
 
-    wait!();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    'main: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                sdl2::event::Event::Quit {..} => break 'main,
+                _ => (),
+            }
+        }
+
+        unsafe { gl::Clear(gl::COLOR_BUFFER_BIT); }
+        
+        window.gl_swap_window();
+    }
 }
