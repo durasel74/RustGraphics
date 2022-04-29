e735631e250d3af9cf146a983b6dbc53c0295075
durasel74 <diman174cheb@mail.ru>
2022-04-29 15:53:21 +0500

    Texture filtering fix

--- src/objects/texture.rs
+++ src/objects/texture.rs
@@ -41,8 +41,8 @@ impl Texture {
             gl::BindTexture(gl::TEXTURE_2D, texture_id);
             gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::REPEAT as i32);
             gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::REPEAT as i32);
-            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::LINEAR as i32);
-            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as i32);
+            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::LINEAR_MIPMAP_LINEAR as i32);
+            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR_MIPMAP_LINEAR as i32);
 
             gl::TexImage2D(gl::TEXTURE_2D, 0, gl::RGB as i32, img_size.0 as i32, 
                 img_size.1 as i32, 0, gl::RGBA, gl::UNSIGNED_BYTE, 
